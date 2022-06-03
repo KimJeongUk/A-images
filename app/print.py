8f5d52199baeba0bef5cdbbfe6f2fd7960bfe302
JeongUk <qjawls65@naver.com>
2022-06-03 16:42:28 +0900

    0603 최종 수정

--- app/print.py
+++ app/print.py
@@ -22,6 +22,7 @@ or file.endswith(".JPG")
 or file.endswith(".bmp")]
 
 
+print("Classify Start ---->")
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
@@ -49,31 +50,87 @@ for x in files:
   #올바른 폴더에 이미지를 저장하기 위한 내용
   usr_db_data = x
 
+  
     #정상
   if((outx.argmax() == 3) or (outx.argmax() == 6) or (outx.argmax() == 9) or (outx.argmax() == 12) or (outx.argmax() == 15) or (outx.argmax() == 18) or (outx.argmax() == 20) or (outx.argmax() == 23) or (outx.argmax() == 25) or (outx.argmax() == 27) or (outx.argmax() == 30) or (outx.argmax() == 33)):
     user_data_path = ("./src/public/predict/normal/"+ str(outx.argmax()) )+ "_"+ usr_db_data  
     state ="정상"
-    print(1)
+    
   #수리
   elif((outx.argmax() == 1) or (outx.argmax() == 4) or (outx.argmax() == 7) or (outx.argmax() == 10) or (outx.argmax() == 13) or (outx.argmax() == 16) or (outx.argmax() == 21) or (outx.argmax() == 28) or (outx.argmax() == 31)):
     user_data_path = ("./src/public/predict/repair/"+ str(outx.argmax()) )+ "_"+ usr_db_data   
     state ="수리"
-    print(2)
+    
   #교체폐기
   elif((ourx.armax() == 2 )or (outx.argmax() == 5) or (outx.argmax() == 8) or (outx.argmax() == 11) or (outx.argmax() == 14) or (outx.argmax() == 17) or (outx.argmax() == 19) or (outx.argmax() == 22) or (outx.argmax() == 24) or (outx.argmax() == 26) or (outx.argmax() == 29) or (outx.argmax() == 32) or (outx.argmax() == 34)):
     user_data_path = ("./src/public/predict/replacement/"+ str(outx.argmax()) )+ "_"+ usr_db_data  
     state = "교체폐기"
-    print(3)
+    
 
-  imagebf.save( user_data_path,'jpeg') 
+  imagebf.save( user_data_path,'jpeg')
+
+  if((outx.argmax() == 0) or (outx.argmax()==1 ) or (outx.argmax()==2)):
+  
+    kind ="가로등"
+  
+  elif((outx.argmax() == 3) or (outx.argmax()==4 ) or (outx.argmax()==5)):
+  
+    kind = "교통신호제어기"
+  
+  elif((outx.argmax() == 6) or (outx.argmax()== 7 ) or (outx.argmax()==8)):
+  
+    kind = "도로안전표지판"
+  
+  elif((outx.argmax() == 9) or (outx.argmax()== 10 ) or (outx.argmax()== 11)):
+  
+    kind = "맨홀"
+  
+  elif((outx.argmax() == 12) or (outx.argmax()== 13 ) or (outx.argmax()== 14)):
+  
+    kind = "지하철환기구"
+  
+  elif((outx.argmax() == 15) or (outx.argmax()== 16 ) or (outx.argmax()== 17)):
+  
+    kind = "트랜치"
+  
+  elif((outx.argmax() == 18) or (outx.argmax()== 19)):
+  
+    kind = "보도블럭"
+  
+  elif((outx.argmax() == 20) or (outx.argmax()== 21 ) or (outx.argmax()==22)):
+  
+    kind = "보차도경계석"
+  
+  elif((outx.argmax() == 23) or (outx.argmax()== 24 )):
+  
+    kind = "장애인주차구역"
+  
+  elif((outx.argmax() == 25) or (outx.argmax()== 26 )):
+  
+    kind = "점자블록"
+  
+  elif((outx.argmax() == 27) or (outx.argmax()== 28 ) or (outx.argmax()==29)):
+  
+    kind = "정류장쉘터"
+  
+  elif((outx.argmax() == 30 ) or (outx.argmax()== 31 ) or (outx.argmax()==32)):
+  
+    kind = "도로안전표지판"
+  
+  elif((outx.argmax() == 33) or (outx.argmax()== 34 )):
+  
+    kind = "지상노출승강기"
+  
 
 #sql 연결
 conn = pymysql.connect(host='127.0.0.1',user="root", password="123123r", db="userprofile",charset='utf8')
 cur = conn.cursor()
-query = 'UPDATE civil SET percent=%s,state=%s WHERE name ="김정욱"'
-vals = ( outx.max()*99, state)
+query = 'UPDATE civil SET percent=%s,state=%s,kind=%s WHERE name ="김정욱"'
+vals = ( outx.max()*99, state, kind)
 cur.execute(query,vals)
 conn.commit()
 conn.close()
 
-print("complete")
+print("<---- Classify Complete")
+
+print("SQL UPDATE Complete")
